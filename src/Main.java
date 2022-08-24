5edc63cfad97243a5baae280c9b42673cb02d9d1
Edgar Figueroa <edgarefigueroa@outlook.com>
2022-08-24 17:20:12 -0400

    drink menu

--- src/Main.java
+++ src/Main.java
@@ -24,7 +24,6 @@ public class Main {
 
         System.out.println("We are deciding on the price for our standard cupcake. Here is the description: ");
         cupcake.type();
-
         System.out.println("How much would you like to charge for the cupcake?" +
                 "\n(Input a numerical number taken to 2 decimal places)");
 
@@ -34,7 +33,6 @@ public class Main {
 
         System.out.println("We are deciding on the price for our red velvet cupcake. Here is the description: ");
         redVelvet.type();
-
         System.out.println("How much would you like to charge for the cupcake?" +
                 "\n(Input a numerical number taken to 2 decimal places)");
 
@@ -44,7 +42,6 @@ public class Main {
 
         System.out.println("We are deciding on the price for our chocolate cupcake. Here is the description: ");
         chocolate.type();
-
         System.out.println("How much would you like to charge for the cupcake?" +
                 "\n(Input a numerical number taken to 2 decimal places)");
 
@@ -55,39 +52,106 @@ public class Main {
         cupcakeMenu.add(cupcake);
         cupcakeMenu.add(redVelvet);
         cupcakeMenu.add(chocolate);
-        //System.out.println(Arrays.toString(cupcakeMenu));
-        //System.out.println(Arrays.deepToString(cupcakeMenu.toArray()));
+
+
+        // new ArrayList which contains drinkMenu
+        ArrayList<Drink> drinkMenu = new ArrayList<Drink>();
+
+        Drink water = new Drink();
+        Soda soda = new Soda();
+        Milk milk = new Milk();
+
+        System.out.println("We are in the middle of creating the drink menu. We currently have three types of drinks on" +
+                "\nthe menu but we need to decide on pricing");
+
+        System.out.println("We are deciding on the price for our bottled water. Here is the description: ");
+        water.type();
+        System.out.println("How much would you like to charge for the water bottle?" +
+                "\n(Input a numerical number taken to 2 decimal places)");
+
+        priceText = input.nextLine();
+        price = Double.parseDouble(priceText);
+        water.setPrice(price);
+
+        System.out.println("We are deciding on the price for our bottle of soda. Here is the description: ");
+        soda.type();
+        System.out.println("How much would you like to charge for the bottled soda?" +
+                "\n(Input a numerical number taken to 2 decimal places)");
+
+        priceText = input.nextLine();
+        price = Double.parseDouble(priceText);
+        soda.setPrice(price);
+
+        System.out.println("We are deciding on the price for our bottle of milk. Here is the description: ");
+        milk.type();
+        System.out.println("How much would you like to charge for the bottled milk?" +
+                "\n(Input a numerical number taken to 2 decimal places)");
+
+        priceText = input.nextLine();
+        price = Double.parseDouble(priceText);
+        milk.setPrice(price);
+
+        // add to drinkMenu
+        drinkMenu.add(water);
+        drinkMenu.add(soda);
+        drinkMenu.add(milk);
+
+        System.out.println("\n---Cupcake Menu---\n");
+        for(Cupcake i : cupcakeMenu) {
+            System.out.println(i.getClass().getSimpleName() + " Price: " + i.getPrice());
+        }
+        System.out.println("\n---Drink Menu---\n");
+        for(Drink i : drinkMenu) {
+            System.out.println(i.getClass().getSimpleName() + " Price: " + i.getPrice());
+        }
 
     }
 
     // new parent class
     static class Cupcake {
-
         public double price;
-
         public double getPrice() {
             return price;
         }
-
         public void setPrice(double price) {
             this.price = price;
         }
-
         public void type() {
             System.out.println("A basic, generic cupcake, with vanilla frosting");
         }
     }
-
     static class RedVelvet extends Cupcake {
         public void type() {
             System.out.println("A red velvet based cupcake, with cream cheese frosting");
         }
     }
-
-    // Create a new class named Chocolate that extends from Cupcake
     static class Chocolate extends Cupcake {
         public void type() {
             System.out.println("A chocolate based cupcake, with chocolate frosting");
+        }
+    }
+
+    // new parent class
+    static class Drink {
+        public double price;
+        public double getPrice() {
+            return price;
+        }
+        public void setPrice(double price) {
+            this.price = price;
+        }
+        public void type() {
+            System.out.println("A bottle of water");
+        }
+    }
+    static class Soda extends Drink {
+        public void type() {
+            System.out.println("A bottle of soda");
+        }
+    }
+    static class Milk extends Drink {
+        public void type() {
+            System.out.println("A bottle of milk");
         }
     }
 
